57fa8099cc12a35908d6701a1eb15137978554b2
hongjun jeon <wjsghdwns730@naver.com>
2018-01-19 17:37:08 +0900

    Final Project

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -4,6 +4,19 @@ from django.contrib import admin
 from .models import Question
 from .models import Product
 
-admin.site.register(Question)
-admin.site.register(Product)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('email', 'question', 'User_url', 'created_time') # 리스트에 이 필드들 추가
+    list_filter = ['created_time'] # 리스트에 필터 추가
+    search_fields = ['email', 'created_time', 'question', 'User_url'] # 검색 필터 추가
+
+
+admin.site.register(Question, QuestionAdmin)  # 두 번째 인자로 위에 만든 model admin class를 넘긴다.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('email', 'search', 'product_name', 'product_place', 'created_time') # 리스트에 이 필드들 추가
+    list_filter = ['created_time'] # 리스트에 필터 추가
+    search_fields = ['email', 'search', 'product_name', 'product_place', 'created_time'] # 검색 필터 추가
+
+admin.site.register(Product, ProductAdmin)
 # Register your models here.
